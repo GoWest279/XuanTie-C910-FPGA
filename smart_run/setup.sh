ab50ea1edee4124d707dc8e1ae84d493a40fdbaa
john-liu2039 <jl@ad2039.com>
2022-04-10 19:34:44 -0500

    sync

---
+++ smart_run/setup.sh
@@ -0,0 +1,3 @@
+export TOOL_EXTENSION=/home/siriux/Z00tools/xuantie-gcc/bin/
+#export TOOL_EXTENSION=/home/siriux/Z00tools/xuantie-gcc/riscv64-unknown-elf/bin/
+export CODE_BASE_PATH=/home/siriux/Z02openc910/C910_RTL_FACTORY/
