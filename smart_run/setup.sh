815a4907d0bbba80a1864f7b1aaf10b6804df33d
john-liu2039 <jl@ad2039.com>
2022-04-17 16:46:40 -0500

    some cleanup

--- smart_run/setup.sh
+++
@@ -1,3 +0,0 @@
-export TOOL_EXTENSION=/home/siriux/Z00tools/xuantie-gcc/bin/
-#export TOOL_EXTENSION=/home/siriux/Z00tools/xuantie-gcc/riscv64-unknown-elf/bin/
-export CODE_BASE_PATH=/home/siriux/Z02openc910/C910_RTL_FACTORY/
